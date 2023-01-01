54846f9f9ec8349dfeda7b939b447dbb96dec99c
Masaharu Inoue <itisagoodcat@gmail.com>
2023-01-01 18:56:11 +0900

    fix: インデント方法変更

--- src/SequenceDiagram/lib/MessageStore.ts
+++ src/SequenceDiagram/lib/MessageStore.ts
@@ -1,31 +1,76 @@
-import { MemberObject } from '../types/index.ts'
+import { MemberObject } from '../types'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { run } from '@mermaid-js/mermaid-cli'
+
+import { promises as fs } from 'fs'
+import path from 'path'
+
+export type Options = {
+  autoNumber: boolean
+  indent: string
+}
+
+export const defaultOptions = {
+  autoNumber: false,
+  indent: '    ',
+}
 
 export class MessageStore {
-  public message: string
-  public isIndented: boolean
+  private message: string
+  private isIndented: boolean
+  private options: Options
+  private indentCount = 0
 
-  constructor(personsObj: MemberObject) {
+  constructor(
+    membersObj: MemberObject,
+    options: Partial<Options> = defaultOptions,
+  ) {
+    this.options = defaultOptions
+    this.options.autoNumber = options.autoNumber
+      ? options.autoNumber
+      : defaultOptions.autoNumber
+    this.options.indent = options.indent
+      ? options.indent
+      : defaultOptions.indent
+
     this.message = `sequenceDiagram\n`
+    this.indentCount = 1
+    if (this.options.autoNumber === true) {
+      this.add(`autonumber`)
+    }
     this.isIndented = false
-    for (const person in personsObj) {
-      this.message = this.message + `  ${personsObj[person]} ${person}\n`
+
+    for (const member in membersObj) {
+      this.add(`${membersObj[member]} ${member}`)
     }
   }
 
   add(msg: string) {
-    if (this.isIndented) {
-      this.message = this.message + '  ' + msg
-    } else {
-      this.message = this.message + msg
-    }
-  }
-  print() {
-    return this.message
+    const indent = this.options.indent
+
+    this.message = this.message + indent.repeat(this.indentCount) + msg + `\n`
   }
   setIndent() {
     this.isIndented = true
+    this.indentCount = this.indentCount + 1
   }
   unsetIndent() {
     this.isIndented = false
+    this.indentCount = this.indentCount - 1
+  }
+
+  toString() {
+    return this.message
+  }
+  toMmd(pathString: string) {
+    return fs.writeFile(pathString, this.toString())
+  }
+
+  async toSvg(pathString: string) {
+    const tmpPath = path.join(__dirname, './_tmp.mmd')
+    await fs.writeFile(tmpPath, this.toString())
+    await run(tmpPath, pathString)
   }
 }
